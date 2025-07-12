05d238e1dd6de44fcaf4f86488be0bad494f7020
d-art3m <didenko.art3m@gmail.com>
2025-07-12 19:56:42 +0300

    add proxy

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { tap, map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class AuthService {
-  private tokenUrl = 'https://platform.fintacharts.com/identity/realms/fintatech/protocol/openid-connect/token';
+  private tokenUrl = '/identity/realms/fintatech/protocol/openid-connect/token';
   private clientId = 'app-cli';
 
   private username = import.meta.env['NG_APP_USERNAME'];
